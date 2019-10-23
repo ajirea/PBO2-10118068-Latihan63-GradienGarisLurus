781db2c1bd847a0d73b7929774abe73e79a6c730
Satria Aji Putra <satriamaxt@gmail.com>
2019-10-23 14:26:45 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan63.gradiengarislurus;
+
+/**
+ *	
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Membuat program dengan konsep abstraksi untuk menghitung
+ * gradien
+ */
+public interface GarisLurus {
+    public int hitungGradien();
+}
